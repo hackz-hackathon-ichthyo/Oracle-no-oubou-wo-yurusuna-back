773488956c465d37a315fc1613bffa8aa7299ad4
yukinissie <bigtree2490@outlook.jp>
2022-06-16 18:07:09 +0900

    change text

--- hackz-server/movie-server/src/test/kotlin/tech/notchman/ApplicationTest.kt
+++ hackz-server/movie-server/src/test/kotlin/tech/notchman/ApplicationTest.kt
@@ -1,26 +1,12 @@
 package tech.notchman
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.auth.*
-import io.ktor.util.*
-import io.ktor.server.plugins.autohead.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.httpsredirect.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.serialization.gson.*
-import io.ktor.server.websocket.*
-import io.ktor.websocket.*
-import java.time.Duration
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import tech.notchman.plugins.*
+import tech.notchman.plugins.configureRouting
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
@@ -30,7 +16,7 @@ class ApplicationTest {
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("Hello World!!", bodyAsText())
         }
     }
 }
